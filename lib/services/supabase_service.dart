37afd69dd57f2d8f75517e3808002f27e03cf45e
vinothpepstech <vinothpepstech@gmail.com>
2025-10-02 14:48:28 +0000

    updated apk with ui changesed

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -1,7 +1,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class SupabaseService {
-  static const String _url = 'https://sxtejkwxnuxkomrdxhvf.supabase.co';
+class SupabaseService {static const String _url = 'https://sxtejkwxnuxkomrdxhvf.supabase.co';
   static const String _key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4dGVqa3d4bnV4a29tcmR4aHZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNjEzNjMsImV4cCI6MjA3NDczNzM2M30.XIpjm6BVx0k2mvTXq_srOIGX17QyAZ6vS1RUMggL3uo';
   static final SupabaseClient _client = SupabaseClient(_url, _key);
 
